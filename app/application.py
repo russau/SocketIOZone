b4fb93e6d7f08571f9fd3c0cecdf50562df6ff6a
Russ Sayers <russell.sayers@gmail.com>
2019-02-03 13:10:54 -0800

    emitting from thread

--- app/application.py
+++ app/application.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
+from threading import Thread
+import time
+
 application = Flask(__name__)
-# application.config['SECRET_KEY'] = 'secret!'
+application.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(application)
+thread = None
+
+def background_thread():
+    print("starting thread")
+    while True:
+        print("emitting from thread")
+        socketio.emit('my response', 'from thread')
+        time.sleep(4)
 
 @application.route('/')
 def index():
@@ -12,6 +23,11 @@ def index():
 @socketio.on('connect')
 def test_connect():
     print('Client connected')
+    socketio.emit('my response', 'from connect')
+    global thread
+    if thread is None:
+        thread = Thread(target=background_thread)
+        thread.start()
 
 @socketio.on('disconnect')
 def test_disconnect():
@@ -24,4 +40,4 @@ def handle_message(message):
 
 
 if __name__ == '__main__':
-    socketio.run(application, debug=True)
+    socketio.run(application, debug=False)
